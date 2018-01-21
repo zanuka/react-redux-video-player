5b44b0a6630dfefe0207825804546e171af90f76
Method <git@zanuka.com>
2018-01-22 02:23:02 +1300

    index setup to render app in container

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>this is fine</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
