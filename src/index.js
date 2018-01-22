31eb6073ff03e8679cdf2cb4142f8ed4f92fd0fa
Method <git@zanuka.com>
2018-01-23 00:26:00 +1300

    wire up video list component and add a vid count

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchInput from './components/search-input';
+import VideoList from './components/video-list';
 const API_KEY = 'AIzaSyBCcRmuoJeaqSFzxoUujWMEv6V1Swp2qmU';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
     return (
       <div>
         <SearchInput />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
